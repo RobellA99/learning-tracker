eef22df177001f361fc44f21c06b9397ed88386b
Robell <robellasfaw@gmail.com>
2025-05-18 15:24:02 +0100

    Add title

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,9 @@
+import "./LandingPage.scss";
+
 export default function LandingPage() {
   return (
-    <section>
-      <h1>Keep Track of What You Learn</h1>
+    <section className="title">
+      <h1 className="title__text">Keep Track of What You Learn</h1>
     </section>
   );
 }
