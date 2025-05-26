9bea30de062b7ea66e15e396cf18ac04bcc9e65e
Robell <robellasfaw@gmail.com>
2025-05-26 19:49:58 +0100

    Add info card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+export default function Card() {
+  return (
+    <>
+      <article>
+        <h2>Categories</h2>
+      </article>
+      <article>
+        <div>
+          <h3>Skill</h3>
+          {/* will show when you expand the card */}
+          <p>Resources</p>
+          <p>Goals</p>
+        </div>
+      </article>
+    </>
+  );
+}
